ba064b40bbcc0a138f0f13c5fa7caa5bf6da5e6b
haroonalbar <haroonalbarx@gamil.com>
2023-04-07 01:51:23 +0530

    added images and icons form cloned repo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import { StyledHeader } from './styled/Header.styled'
+import { Container } from './styled/Container.styled'
 
 type Props = {}
 
 const Header = (props: Props) => {
   return (
-    <StyledHeader bg='yellow'>
-        <h1>Header</h1>
+    <StyledHeader>
+        <Container>
+            <h1>Header</h1>
+        </Container>
     </StyledHeader>
   )
 }
